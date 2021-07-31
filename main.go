3f46240b77428e72e3556e7db4a96c9971671342
azazana <azazana@list.ru>
2021-07-31 23:27:09 +0300

    errors

--- main.go
+++ main.go
@@ -7,19 +7,18 @@ import (
 type ErrNegativeSqrt float64
 
 func (e ErrNegativeSqrt) Error() string {
-	return fmt.Sprintf("cannot Sqrt negative number: %g", float64(e))
+	return fmt.Sprintf("cannot Sqrt negative number: %q", float64(e))
 }
-func Sqrt(x float64) (float64, error) {
+
+func Sqrt(x float64) (float64 error) {
 	if x < 0 {
 		return 0, ErrNegativeSqrt(x)
 	}
-	z := 1.0
-	for i := 0; i < 10; i++ {
-		z = z - ((z*z)-x)/(2*z)
-	}
-	return z, nil
+
+	return 0, nil
+
 }
 func main() {
-	fmt.Println(Sqrt(2))
-	fmt.Println(Sqrt(-2))
+	//fmt.Println(Sqrt((2)))
+	//fmt.Println(Sqrt(-2))
 }
