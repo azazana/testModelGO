d07367c64cbb29f294d4f2be21360e05ea6aa7fe
azazana <azazana@list.ru>
2021-08-04 22:29:56 +0300

    My Tree

--- Channel/Channel.go
+++ Channel/Channel.go
@@ -1,27 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func fibonacci(c, quit chan int) {
-	x, y := 0, 1
+func main() {
+	tick := time.Tick(100 * time.Millisecond)
+	boom := time.After(500 * time.Millisecond)
 	for {
 		select {
-		case c <- x:
-			x, y = y, x+y
-		case <-quit:
-			fmt.Println("quit")
+		case <-tick:
+			fmt.Println("tick.")
+		case <-boom:
+			fmt.Println("BOOM!")
 			return
+		default:
+			fmt.Println(" .")
+			time.Sleep(50 * time.Millisecond)
 		}
 	}
-}
-func main() {
-	c := make(chan int)
-	quit := make(chan int)
-	go func() {
-		for i := 0; i < 10; i++ {
-			fmt.Println(<-c)
-		}
-		quit <- 0
-	}()
-	fibonacci(c, quit)
 }
---
+++ tree/Mytree.go
@@ -0,0 +1,49 @@
+package main
+import (
+"https://github.com/golang/tour"
+"fmt"
+)
+
+func Walc (t *tree.Tree, ch chan int) {
+	recWalc(t, ch)
+	close(ch)
+}
+
+func recWalc(t *tree.Tree, ch chan int) {
+	if t !=nil {
+		recWalc(t.Left, ch)
+		ch<-t.Value
+		recWalc(t.Right, ch)
+	}
+}
+func Same(t1,t2 *tree.Tree) bool {
+	ch1 := make (chan int)
+	ch2 := make (chan int)
+	go Walc(t1,ch1)
+	go Walc(t1,ch2)
+
+	for {
+		x1,ok1 :=<-ch1
+		x2,ok2 :=<-ch2
+		switch {
+		case ok1 != ok2:
+			return false
+		case !ok1:
+			return true
+		case x1 !=x2:
+			return false
+			default
+			
+		}
+	}
+}
+
+func main() {
+	ch := make(chan int)
+	go Walc(tree.New(1),ch)
+	for v := range ch{
+		fmt.Println(v)
+	}
+	fmt.Println(Same(tree.New(1),tree.New(1)))
+	fmt.Println(Same(tree.New(1),tree.New(2)))
+}
